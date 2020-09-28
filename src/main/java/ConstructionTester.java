663d443ed439e91d43702ce6705487609d75ad9c
Nebeu <nebeu.241684@nv.ccsd.net>
2020-09-28 07:33:07 +0000

    I added code

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,35 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
-    }
-}
+        Scanner in = new Scanner (System.in);
+        
+        //variables
+        int numBoards;
+        int numWindows;
+        double taxRate;
+        double total, grandTotal;
+        double lumCost = 8;;
+        double winCost = 11;
+      
+
+        System.out.print("Enter the sales tax rate: ");
+        taxRate = scanner.nextDouble();
+        taxRate = in.nextDouble();
+        
+        System.out.println("How many boards do you need? ");
+        numBoards = scanner.nextInt();
+        numBoards = in.nextDouble();
+
+        System.out.println("How many windows do you need? ");
+        numWindows = scanner.nextInt();
+
+
+        Construction myConstruction = new Construction (taxRate, lumCost, winCost);
+
+        total = test.lumCost (numBoards) + test.winCost (numWindows);
+        System.out.print("The total is: " + total);
+
+        grandTotal = test.grandTotal (total);
+        System.out.print("The grand total is: " + grandTotal);
+    }//ends main method
+}//ends class
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,32 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
-    }
-}
+        double startingLat, startingLong, endingLat, endLong, distance;
+
+        //the user input and variables 
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Put the starting latitude of the location: ")
+        startingLat = in.nextDouble();
+
+        System.out.print("Put the starting longitude of the location: ")
+        startingLong = in.nextDouble();
+
+        System.out.print("Put the ending latitude of the location: ")
+        endingLat = in.nextDouble();
+
+        System.out.print("Put the ending longitude of the location: ")
+        endingLong = in.nextDouble();
+
+
+        //establishes the starting and ending GeoLocation
+        GeoLocation startingLoc = new GeoLocation (startingLat, startingLong);
+
+        GeoLocation endingLoc = new GeoLocation (endingLat, endingLong);
+
+
+        //establishes the distance
+        distance = startingLoc.distanceFrom (endingLoc);
+         System.out.print("The distance is " + distance + " miles.");
+    }//ends main method
+}//ends class
